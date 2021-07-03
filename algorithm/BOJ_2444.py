f3462c93bac6ae587614e99df0efa80060bed819
= <=>
2021-07-03 14:13:45 +0900

    21-07-03

---
+++ algorithm/BOJ_2444.py
@@ -0,0 +1,17 @@
+
+
+N = int(input())
+
+for i in range(1,N+1):
+    for j in range(i,N):
+        print(" ",end="")
+    for j in range(0,2*i-1):
+        print("*",end="")
+    print("")
+
+for i in range(1,N):
+    for j in range(0,i):
+        print(" ",end="")
+    for j in range(1,2*N-2*i):
+        print("*",end="")
+    print("")
