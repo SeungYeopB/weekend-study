03b32817b53c7d934617945b0384dc5d5f321859
SEUNGYEOPBAEK <study0610@naver.com>
2021-07-15 17:07:53 +0900

    21-15-07

---
+++ algorithm/free1_210715.py
@@ -0,0 +1,9 @@
+""" 다음 코드는 inch 단위의 자료를 입력받아 cm단위를 구하는 예제입니다. 빈칸에 알맞은
+내용은 넣어 코드를 완성해주세요. """
+
+str_input = input("숫자 입력>")
+num_input = float(str_input)
+
+print()
+print(num_input, "inch")
+print((num_input * 2.54), "cm")
